9f653ed9e6f658438e00a208329bdda12bdf262a
xiaojin <603611653@qq.com>
2019-03-26 18:33:07 +0800

    'fix'

--- app01/models.py
+++ app01/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class UserInfo(models.Model):
-    id = models.AutoField(primary_key=True)               #创建一个自增主键
-    name = models.CharField(null=False,max_length=20)     #创建一个VARCHAR类型不能为空的字段
-    password = models.CharField(null=False,max_length=20)
+class publisher_list(models.Model):
+    publisher_id = models.AutoField(primary_key=True)
+    publisher_name = models.CharField(null=True,max_length=64)
 
 class Book_list(models.Model):
     book_id = models.AutoField(primary_key=True)
     book_name = models.CharField(null=True,max_length=64)
     book_description = models.CharField(null=True,max_length=64)
     book_country = models.CharField(null=True,max_length=64)
+    book_publish = models.ForeignKey(publisher_list,to_field='publisher_id',on_delete='CASCADE')
